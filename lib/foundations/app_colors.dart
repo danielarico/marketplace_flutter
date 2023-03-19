13bff2a40e11f2b72c8f0faf104d434e733d12a0
Daniela Rico <dani.5991@gmail.com>
2023-03-19 01:54:49 -0500

    First commit

---
+++ lib/foundations/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color PURPLE = Color(0xff9c89b8);
+  static const Color PINK = Color(0xfff0a6ca);
+  static const Color LIGHT_PINK = Color(0xffefc3e6);
+  static const Color CREAM = Color(0xfff0e6ef);
+  static const Color LAVENDER = Color(0xffb8bedd);
+  static const Color WHITE = Color(0xffffffff);
+  static const Color BLACK = Color(0xff000000);
+  static const Color GREY = Color(0xffc5c3c6);
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:marketplace_flutter/foundations/app_colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: AppColors.PURPLE,
+        appBarTheme: const AppBarTheme(color: AppColors.PURPLE),
+        scaffoldBackgroundColor: AppColors.CREAM,
+        buttonTheme: const ButtonThemeData(
+          buttonColor: AppColors.PURPLE,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: AppColors.PURPLE,
+        ),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
